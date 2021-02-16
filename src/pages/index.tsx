a9f376dc2b039361a8d4f4248989c9d1d81149ba
Sanjay Selvadevan <71178439+AlbertSanSun@users.noreply.github.com>
2021-02-16 08:43:11 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,35 +5,18 @@ import Features from '../components/features'
 import GitHub from '../components/svgs/github'
 import sharedStyles from '../styles/shared.module.css'
 
-<div className="explanation">
-        <p>
-          This is a statically generated{' '}
-          <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-          <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-          is deployed with <ExtLink href="https://vercel.com">Vercel</ExtLink>.
-          It leverages some upcoming features in Next.js like{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/9524">
-            SSG support
-          </ExtLink>{' '}
-          and{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/8626">
-            built-in CSS support
-          </ExtLink>{' '}
-          which allow us to achieve all of the benefits listed above including
-          blazing fast speeds, great local editing experience, and always being
-          available!
-        </p>
-          Now about me.
-          My real name is Sanjay, but I would really like you to call me SanSun or AlbertSanSun on the Internet.
-          I am currently in grade 8th and currently learning python, JS, HTML. I love Maths and Science and 
-          I'm really good at them. I have created many Youtube Channels (and deleted a lot of them, because I couldn't manage
-          them. But I am sure I will manage the last one.) Most of them where based on Maths and Science, but this one will be special...
-          Look at my Github Page for more facts and stuff...
-          I will upload my videos sometime during April and May, stay tuned. (is the word "tuned" the right word to be used in this sentence?)
+export default () => (
+  <>
+    <Header titlePre="Home" />
+    <div className={sharedStyles.layout}>
+      <img src="/demo.png" width="60%" alt="page top" />
+      <h1>Koji Notion Blog</h1>
+      <h2></h2>
 
-        <p>
-          
-        </p>
+      {/* <Features /> */}
+
+      <div className="explanation">
+        <p>About this blog</p>
       </div>
     </div>
   </>
