ed9cbd198b3ebb4961975012f43e93f750113124
Sanjay Selvadevan <71178439+AlbertSanSun@users.noreply.github.com>
2021-02-16 09:00:41 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,13 +10,22 @@ export default () => (
     <Header titlePre="Home" />
     <div className={sharedStyles.layout}>
       <img src="/demo.png" width="60%" alt="page top" />
-      <h1>Koji Notion Blog</h1>
+      <h1>SanSun Website</h1>
       <h2></h2>
 
       {/* <Features /> */}
 
       <div className="explanation">
-        <p>About this blog</p>
+        <p>About this site:
+           Go to the site's Github Repository.
+          
+          Now about the owner of this site:
+          This site owner is me Sanjay but I would like you to call me SanSun or AlbertSanSun on the Internet.(Unless ur my Relatives or Friends)
+          I am currently in grade 8th and currently learning python, JS, HTML. I love Maths and Science and 
+          I'm really good at them. I have created many Youtube Channels (and deleted a lot of them, because I couldn't manage
+          them. But I am sure I will manage the last one.) Most of them where based on Maths and Science, but this one will be special...
+          I love Potatoes and hate Brijals.
+        </p>
       </div>
     </div>
   </>
