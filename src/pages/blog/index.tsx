5928f51d002e9e1b8ea0d19049105bc8e2ca0227
Sanjay Selvadevan <71178439+AlbertSanSun@users.noreply.github.com>
2021-02-18 13:54:26 +0530

    Update index.tsx

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -63,7 +63,7 @@ export default ({ posts = [], preview }) => {
         </div>
       )}
       <div className={`${sharedStyles.layout} ${blogStyles.blogIndex}`}>
-        <h1>My Notion Blog</h1>
+        <h1>My Blog</h1>
         {posts.length === 0 && (
           <p className={blogStyles.noPosts}>There are no posts yet</p>
         )}
@@ -88,7 +88,7 @@ export default ({ posts = [], preview }) => {
               )}
               <p>
                 {(!post.preview || post.preview.length === 0) &&
-                  'Go inside!'}
+                  ''}
                 {(post.preview || []).map((block, idx) =>
                   textBlock(block, true, `${post.Slug}${idx}`)
                 )}
